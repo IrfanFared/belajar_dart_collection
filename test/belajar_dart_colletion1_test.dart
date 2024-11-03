fe0d721ba65dfc1244b5288f846923c4cc2f83e7
Muhammad Irfan Pratama <160725399+IrfanFared@users.noreply.github.com>
2024-11-03 11:20:50 +0700

    hari pertama belajar dart colletion

---
+++ test/belajar_dart_colletion1_test.dart
@@ -0,0 +1,8 @@
+import 'package:belajar_dart_colletion1/belajar_dart_colletion1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
